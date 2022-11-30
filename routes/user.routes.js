b7e769d709098f798ecb820e0509e7ebf6f778b1
Łukasz Wartałowicz <wartalowicz.lukasz.praca@gmail.com>
2022-11-30 23:09:49 +0100

    Show name and avatar

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const router = express.Router();
+
 router.get('/logged', (req, res) => {
   if (req.user) {
-    res.render('logged');
+    console.log('useR', req.user);
+    res.render('logged', {name: req.user.name.givenName, photo: req.user.photos[0].value});
   }
   else res.redirect('/user/no-permission');
 });
+
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
 });
+
 router.get('/profile', (req, res) => {
   if (req.user) {
     res.render('profile');
   }
   else res.redirect('/user/no-permission');
 });
+
 router.get('/profile/settings', (req, res) => {
   if (req.user) {
     res.render('profileSettings');
