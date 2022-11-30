4126845db7d0e449025718d637388fc904c6b351
Łukasz Wartałowicz <wartalowicz.lukasz.praca@gmail.com>
2022-11-30 18:35:29 +0100

    Upgrade project

---
+++ routes/auth.routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+router.get('/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] }));
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
+  (req, res) => {
+    res.redirect('/user/logged');
+  }
+);
+
+router.post('/auth.logout', function (req, res, next) {
+  req.logout(function (err) {
+    if (err) return next(err);
+    res.redirect('auth/logout');
+  });
+});
+
+router.get('/logout', (req, res) => {
+  res.render('logout');
+});
+
+module.exports = router;
